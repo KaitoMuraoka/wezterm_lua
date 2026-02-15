f9212599a88eca2998e81aad885901b1b343c91a
KaitoMuraoka <kaitoboshi@icloud.com>
2026-02-15 15:12:54 +0900

    fix string

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require(wezterm)
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -14,12 +14,11 @@ config.initial_rows = 24 -- default
 config.font_size = 17 -- default size
 
 -- retro fonts
-local VT220 = Glass TTY VT220
-local apple2 = Print Char 21
-local ankomochi = Nu Anko Mochi
+local VT220 = "Glass TTY VT220"
+local apple2 = "Print Char 21"
+local ankomochi = "Nu Anko Mochi"
 
 config.font = wezterm.font(VT220)
 
 -- Finally, return the configuration to wezterm:
 return config
-
