bc8b1fb2e2f10347f8e461de7e6828da99a0cf9b
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-02-16 10:27:09 +0900

    透明度とブラーを追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
+config.automatically_reload_config = true
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
@@ -12,6 +12,8 @@ config.initial_rows = 24 -- default
 
 -- or, changing the font size and color scheme.
 config.font_size = 17 -- default size
+config.window_background_opacity = 0.5
+config.macos_window_background_blur = 20
 
 -- retro fonts
 local VT220 = "Glass TTY VT220"
