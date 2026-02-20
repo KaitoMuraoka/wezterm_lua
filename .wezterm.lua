8761af0b75339307c7772d50915022a3a35e2fb2
KaitoMuraoka <kaitoboshi@icloud.com>
2026-02-20 22:20:37 +0900

    黄緑とか入れてさらにわかりやすく

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,5 +20,28 @@ local ankomochi = "Nu Anko Mochi"
 
 config.font = wezterm.font(VT220)
 
--- Finally, return the configuration to wezterm:
+config.colors = {
+	foreground = "#00ff00",
+
+	ansi = {
+		"#0a2a0a", -- black
+		"#00ff66", -- red
+		"#00ff00", -- green
+		"#88ff00", -- yellow
+		"#00aa66", -- blue
+		"#66ff99", -- magenta
+		"#00ffaa", -- cyan
+		"#ccffcc", -- white
+	},
+	brights = {
+		"#226622", -- bright black
+		"#33ff88", -- bright red
+		"#55ff55", -- bright green
+		"#aaff33", -- bright yellow
+		"#33cc88", -- bright blue
+		"#88ffbb", -- bright magenta
+		"#33ffdd", -- bright cyan
+		"#eeffee", -- bright white
+	},
+}
 return config
