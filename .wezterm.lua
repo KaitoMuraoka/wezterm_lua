a2116a0b73ee3aeb35b5d3a28cb6e433916153ec
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-03-02 19:39:18 +0900

    タブの修正

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,6 +19,21 @@ config.window_background_opacity = 0.7
 -- ブラー
 config.macos_window_background_blur = 30
 
+-- タイトルバーを非表示にする（枠ごと消す）
+config.window_decorations = "RESIZE|TITLE"
+
+-- タブが1枚の時はタブバーを非表示
+-- 複数タブを開いた時は自動で表示される
+config.hide_tab_bar_if_only_one_tab = true
+
+-- 余白も透過に馴染ませる
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 -- retro fonts
 local VT220 = "Glass TTY VT220"
 local apple2 = "Print Char 21"
