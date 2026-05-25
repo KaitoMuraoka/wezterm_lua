adce84747cb86ba8e51464dde9c3e2d382b8e443
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-05-25 11:14:55 +0900

    ターミナルのフォントを変更 (#16)

--- .wezterm.lua
+++ .wezterm.lua
@@ -154,7 +154,7 @@ config.keys = {
    }
  end)
 
-config.font = wezterm.font("HackGen", { weight = "Medium" })
+config.font = wezterm.font("HackGen Console", { weight = "Medium" })
 
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
